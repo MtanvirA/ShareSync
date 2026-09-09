b61d758e74956060633ecd4268967e57309af29f
Md. Tanvir Anjum <tanviranjum201@gmail.com>
2026-09-10 00:22:41 +0600

    feat: create watchlists table and seed data

--- database/oracle/schema/11_test_tables.sql
+++ database/oracle/schema/11_test_tables.sql
@@ -1,3 +1,9 @@
-SELECT *
-FROM portfolios
-ORDER BY portfolio_id;
+SELECT
+    w.watchlist_id,
+    w.watchlist_name,
+    u.user_id,
+    u.name AS user_name
+FROM watchlists w
+JOIN app_users u
+    ON w.user_id = u.user_id
+ORDER BY w.watchlist_id;
---
+++ database/oracle/seed/05_insert_watchlists.sql
@@ -0,0 +1,23 @@
+INSERT INTO watchlists (
+    user_id,
+    watchlist_name,
+    description
+)
+VALUES (
+    1,
+    'Technology Stocks',
+    'Technology companies to monitor'
+);
+
+INSERT INTO watchlists (
+    user_id,
+    watchlist_name,
+    description
+)
+VALUES (
+    1,
+    'Dividend Stocks',
+    'Companies with attractive dividend potential'
+);
+
+COMMIT;
